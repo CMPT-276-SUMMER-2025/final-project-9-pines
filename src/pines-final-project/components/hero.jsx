52bedd0030a0f051c5aaf6a7b754f73c87708935
Sven Jensen <sven@analysis.net>
2025-06-13 22:32:28 -0700

    Add initial nextJS codebase setup

---
+++ src/pines-final-project/components/hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+        <h1 className="text-4xl font-bold text-center text-gray-800">
+            Welcome to the Speech to Gym Logs app
+        </h1>
+    </div>
+);
+
+export default Hero;
